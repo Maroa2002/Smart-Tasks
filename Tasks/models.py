413cc8d898cc0475749d48dcb6f09a3d2977d2ac
Maroa2002 <mgm.engineeringtie847@gmail.com>
2023-03-13 09:13:56 +0300

    second commit

--- Tasks/models.py
+++ Tasks/models.py
@@ -7,3 +7,6 @@ class Tasks(models.Model):
     task_details = models.TextField()
     due_date = models.DateField(default='2000-01-01')
     time_created = models.DateTimeField(auto_now_add=True)
+
+
+# this is a django model
