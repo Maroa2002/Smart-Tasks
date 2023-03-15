ac257c976c9a2b54d40a6c0dc1eb5fc4c84f7139
Maroa2002 <mgm.engineeringtie847@gmail.com>
2023-03-15 21:41:53 +0300

    another commit

--- Tasks/models.py
+++ Tasks/models.py
@@ -7,13 +7,20 @@ from django.db import models
 #     def __str__(self):
 #         return self.title
 
+TASK_TYPE_CHOICES = [
+    ('personal', 'personal'),
+    ('work', 'work'),
+    ('wishlist', 'wishlist'),
+    ('school', 'school')
+]
+
 class Tasks(models.Model):
     task_title = models.CharField(max_length=200)
     # task_type = models.ForeignKey(TaskType, on_delete=models.CASCADE)
     # , null=True, blank=True
-    task_type = models.CharField(max_length=100)
+    task_type = models.CharField(max_length=100, choices=TASK_TYPE_CHOICES)
     task_details = models.TextField()
-    due_date = models.DateField(default='2000-01-01')
+    due_date = models.DateField(default='2023-03-01')
     time_created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
